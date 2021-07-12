70f296543a349115788bbb1a00c17dc84db0f8b5
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-07-13 03:00:46 +0530

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*1fyj8wofj$qa$!_)551$#4y$mt&x+i8$)h9b31tq&schbkdn-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
