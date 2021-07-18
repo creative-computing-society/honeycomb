b6e301bcdbcd33e027b59315e22b882d5f1c8ebb
rdotjain <rupanshijain45678@gmail.com>
2021-07-19 04:57:40 +0530

    api permission changed

--- config/settings.py
+++ config/settings.py
@@ -119,9 +119,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
