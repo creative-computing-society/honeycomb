c44f4487dff22c95805b8fe78477e2ed7801e052
rdotjain <rupanshijain45678@gmail.com>
2021-07-19 05:55:57 +0530

    fixed password saving bug

--- registration/forms.py
+++ registration/forms.py
@@ -9,4 +9,9 @@ class TeamForm(forms.ModelForm):
 class ParticipantForm(forms.ModelForm):
     class Meta:
         model = Participant
-        fields = ['name', 'email', 'mobile', 'team']
+        fields = ['name', 'email', 'mobile', 'team', 'password']
+
+    def save(self, *args, **kwargs):
+        member = super(ParticipantForm, self).save(*args, **kwargs)
+        member.set_password(self.cleaned_data["password"])
+        member.save()
