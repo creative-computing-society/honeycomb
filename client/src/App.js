9693378cbf0b591d1ea6aa6318d459c9ed7b3930
gunjeevsingh <gunjeev.arneja@gmail.com>
2021-07-24 02:39:50 +0530

    random

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,15 @@ import Login from './screens/Login';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Provider} from 'react-redux';
 import store from './store';
+import Landing from './screens/landing';
 
 function App() {
   return (
     <Provider store={store}>
     <Router>
+    <Route exact path='/' component = {Landing}></Route>
       <Route path='/login' component = {Login}/>
+
     </Router>
     </Provider>
   );
---
+++ client/src/screens/landing.js
@@ -0,0 +1,58 @@
+import main from './'
+
+
+const Landing = () => {
+  return (
+    <div className="maincon">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+          <img src="../public/Screenshot_2021-07-18_at_10.28.00_PM.png" alt="" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link disabled"
+                  href="#"
+                  tabIndex={-1}
+                  aria-disabled="true"
+                >
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Landing;
