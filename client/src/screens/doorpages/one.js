f32fd2f88962a138d8749f74dc4c1032cb723999
gunjeevsingh <gunjeev.arneja@gmail.com>
2021-08-03 23:57:52 +0530

    adds Link to one door page

--- client/src/screens/doorpages/one.js
+++ client/src/screens/doorpages/one.js
@@ -1,13 +1,16 @@
 import DoorUI from "../../components/Portal/portal";
 import logo from "../../images/logo.png";
 import "./main.css";
+import { Link } from "react-router-dom";
 const OneDoor = () => {
   return (
     <div className="body">
       <img src={logo} alt="" className="bgimg" />
       <center>
         <div className="singledoor">
-          <DoorUI className="door"></DoorUI>
+          <Link to="/">
+            <DoorUI className="door"></DoorUI>
+          </Link>
         </div>
       </center>
     </div>
