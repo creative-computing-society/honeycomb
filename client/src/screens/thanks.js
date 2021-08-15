4bec5a35c1836027c393cbbd0fd86a3c2bbf09a9
gunjeevsingh <gunjeev.arneja@gmail.com>
2021-08-15 22:59:33 +0530

    fin.

--- client/src/screens/thanks.js
+++ client/src/screens/thanks.js
@@ -21,19 +21,19 @@ const ThankYou = () => {
                 </h3>
             </center>
             <center className='icons'>
-                <a href='www.instagram.com/ccs_tiet/' className='social-icons-thanks'>
+                <a href='www.instagram.com/ccs_tiet/' target="_blank" rel="noopener noreferrer" className='social-icons-thanks'>
                     <InstagramIcon fontSize='large'/>
 
                 </a >
-                <a href='https://www.linkedin.com/company/ccs-tiet/mycompany/' className="social-icons-thanks">
+                <a href='https://www.linkedin.com/company/ccs-tiet/mycompany/' target="_blank" rel="noopener noreferrer" className="social-icons-thanks">
                 <LinkedInIcon fontSize='large'/>
                     
                 </a >
-                <a href='https://www.youtube.com/channel/UCc-F6rlsDdHAKfSiPJEnDLg' className="social-icons-thanks">
+                <a href='https://www.youtube.com/channel/UCc-F6rlsDdHAKfSiPJEnDLg' target="_blank" rel="noopener noreferrer" className="social-icons-thanks">
                 <YouTubeIcon fontSize='large'/>
                     
                 </a >
-                <a href='https://github.com/creative-computing-society' className="social-icons-thanks">
+                <a href='https://github.com/creative-computing-society' target="_blank" rel="noopener noreferrer" className="social-icons-thanks">
                 <GitHubIcon fontSize='large'/>
                 </a>
             </center>
